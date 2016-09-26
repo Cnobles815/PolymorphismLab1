f358accd01b5ca55bb93d73f250e2354267bd586
cnobles <cnobles815@gmail.com>
2016-09-26 18:06:29 -0400

    Mnamesjeff.

--- src/test/java/nobles/christopher/PolymorphismLab1/UserInputTESTS.java
+++ src/test/java/nobles/christopher/PolymorphismLab1/UserInputTESTS.java
@@ -1,7 +1,18 @@
 package nobles.christopher.PolymorphismLab1;
-
+import org.junit.Test;
+import java.util.Scanner;
+import static org.junit.Assert.assertEquals;
 /**
  * Created by christophernobles on 9/26/16.
  */
 public class UserInputTESTS {
+    //I started writing this test and then realized I can't really test this.
+    //In any case, m'name is stll jeff. JEFF.
+
+//    @Test
+//    public void promptStringTEST(){
+//        UserInput userinput = new UserInput();
+//        String expectedValue = "Myname's Jeff.";
+//        String actualValue = userinput.promptString(user)
+//    }
 }
